224145a0e51dec64d4ac00df3254de51b84ce8e6
ahdev <53409839+ahdesenvolvimento@users.noreply.github.com>
2022-02-14 08:43:43 -0300

    Update Navbar.js

--- react/src/components/layout/Navbar.js
+++ react/src/components/layout/Navbar.js
@@ -82,142 +82,6 @@ export default function NavBar({ token, statusNav, setStatusNav }) {
       .catch((error) => console.log(error));
   };
   return (
-    // <nav className="navbar navbar-expand-lg navbar-light">
-    //   <div className="container">
-    //     <Link to="/" className="navbar-brand text-white fw-bold">
-    //       Compra de Cursos
-    //     </Link>
-    //     <button
-    //       className="navbar-toggler"
-    //       type="button"
-    //       data-bs-toggle="collapse"
-    //       data-bs-target="#navbarNavAltMarkup"
-    //       aria-controls="navbarNavAltMarkup"
-    //       aria-expanded="false"
-    //       aria-label="Toggle navigation"
-    //     >
-    //       <span className="navbar-toggler-icon"></span>
-    //     </button>
-    //     <div
-    //       className="collapse navbar-collapse justify-content-end"
-    //       id="navbarNavAltMarkup"
-    //     >
-    //       <div className="navbar-nav">
-    //         {token ? (
-    //           <>
-    //             <Link
-    //               to="/categories"
-    //               className="nav-link active text-white fw-bold"
-    //             >
-    //               Categorias
-    //             </Link>
-    //             <Link
-    //               to="/orders"
-    //               className="nav-link active text-white fw-bold"
-    //             >
-    //               Meu pedidos
-    //             </Link>
-    //             <Link
-    //               to="/courses"
-    //               className="nav-link active text-white fw-bold"
-    //             >
-    //               Criar curso
-    //             </Link>
-    //             <a
-    //               className="nav-link active text-white fw-bold"
-    //               onClick={handleShow}
-    //               aria-current="page"
-    //             >
-    //               Carrinho
-    //             </a>
-    //             <a
-    //               className="nav-link active text-white fw-bold"
-    //               onClick={logout}
-    //               aria-current="page"
-    //             >
-    //               Logout
-    //             </a>
-    //           </>
-    //         ) : (
-    //           <>
-    //             <Link
-    //               to="/login"
-    //               className="nav-link active text-white fw-bold"
-    //             >
-    //               Login
-    //             </Link>
-    //             <Link
-    //               to="/register"
-    //               className="nav-link active text-white fw-bold"
-    //             >
-    //               Cadastrar
-    //             </Link>
-    //           </>
-    //         )}
-    //       </div>
-    //     </div>
-    //   </div>
-    //   <Modal show={show} onHide={handleClose}>
-    //     <Modal.Header closeButton>
-    //       <Modal.Title>Carrinho</Modal.Title>
-    //     </Modal.Header>
-    //     <Modal.Body>
-    //       <div className="table-responsive">
-    //         <table className="table">
-    //           <thead>
-    //             <tr>
-    //               <th>Nome</th>
-    //               <th>Preço</th>
-    //               <th>Descrição</th>
-    //               <th>Categoria</th>
-    //               <th>Ações</th>
-    //               <th>Total</th>
-    //             </tr>
-    //           </thead>
-    //           <tbody>
-    //             {cart.map((item) => (
-    //               <tr key={item.id}>
-    //                 <td>{item.id_courses.name}</td>
-    //                 <td>{item.id_courses.price.toFixed(2)}</td>
-    //                 <td>{item.id_courses.description}</td>
-    //                 <td>{item.id_courses.id_category}</td>
-    //                 <td>
-    //                   <button
-    //                     type="button"
-    //                     className="btn btn-danger"
-    //                     onClick={(e) => removeItemCart(item.id)}
-    //                   >
-    //                     Remover
-    //                   </button>
-    //                 </td>
-    //               </tr>
-    //             ))}
-    //           </tbody>
-    //           <tfoot>
-    //             <tr>
-    //               <td>Total</td>
-    //               <td></td>
-    //               <td></td>
-    //               <td></td>
-    //               <td></td>
-    //               <td>R${total}</td>
-    //             </tr>
-    //           </tfoot>
-    //         </table>
-    //       </div>
-    //     </Modal.Body>
-    //     <Modal.Footer>
-    //       <Button variant="primary">
-    //         <Link to="/order" className="text-white">
-    //           Finalizar pedido
-    //         </Link>
-    //       </Button>
-    //       <Button variant="secondary" onClick={handleClose}>
-    //         Fechar
-    //       </Button>
-    //     </Modal.Footer>
-    //   </Modal>
-    // </nav>
     <Navbar bg="" expand="lg">
       <Container>
         <Navbar.Brand>
@@ -302,6 +166,66 @@ export default function NavBar({ token, statusNav, setStatusNav }) {
           </Nav>
         </Navbar.Collapse>
       </Container>
+      <Modal show={show} onHide={handleClose}>
+         <Modal.Header closeButton>
+           <Modal.Title>Carrinho</Modal.Title>
+         </Modal.Header>
+         <Modal.Body>
+           <div className="table-responsive">
+             <table className="table">
+               <thead>
+                 <tr>
+                   <th>Nome</th>
+                   <th>Preço</th>
+                   <th>Descrição</th>
+                   <th>Categoria</th>
+                   <th>Ações</th>
+                   <th>Total</th>
+                 </tr>
+               </thead>
+               <tbody>
+                 {cart.map((item) => (
+                  <tr key={item.id}>
+                    <td>{item.id_courses.name}</td>
+                    <td>{item.id_courses.price.toFixed(2)}</td>
+                    <td>{item.id_courses.description}</td>
+                    <td>{item.id_courses.id_category}</td>
+                    <td>
+                      <button
+                        type="button"
+                        className="btn btn-danger"
+                        onClick={(e) => removeItemCart(item.id)}
+                      >
+                        Remover
+                      </button>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+              <tfoot>
+                <tr>
+                  <td>Total</td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td>R${total}</td>
+                </tr>
+              </tfoot>
+            </table>
+          </div>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="primary">
+            <Link to="/order" className="text-white">
+              Finalizar pedido
+            </Link>
+          </Button>
+          <Button variant="secondary" onClick={handleClose}>
+            Fechar
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </Navbar>
   );
 }
